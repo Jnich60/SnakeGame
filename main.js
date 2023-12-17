538e13d533b19f192dbd93113b60b5f9e8451906
Jordon <Jnich09@hotmail.com>
2023-12-17 13:35:16 -0600

    Html,JS and CSS files add

---
+++ main.js
@@ -0,0 +1,26 @@
+// Define HTML elements
+const board = document.getElementById('game-board');
+
+// Define game variables
+let snake = [{x: 10, y: 10}];
+
+// Draw game map, snake, food
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+}
+
+// Draw snake
+function drawSnake() {
+    snake.forEach((segment) => {
+        const snakeElement = createGameElement('div', 'snake');
+    })
+}
+
+// Create a snake or food cube/div
+
+function createGameElement(tag, className){
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
